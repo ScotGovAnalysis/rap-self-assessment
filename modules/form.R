54793f747690f80e01ece045d47291699643fe9d
alicebyers5 <alice.byers@gov.scot>
2024-02-01 17:22:28 +0000

    Reactive list of callModules

--- modules/form.R
+++ modules/form.R
@@ -1,16 +1,7 @@
 
-form_ui <- function(id, criteria_data, levels, options) {
+form_ui <- function(id, criteria, options) {
   
   ns <- NS(id)
-  
-  # Check that data has required variables
-  exp_vars <- c("id", "level", "criteria")
-  if(any(!exp_vars %in% names(criteria_data))) {
-    cli::cli_abort(
-      paste("{.var criteria_data} is missing at least one of the following",
-            "variables: {exp_vars}.")
-    )
-  }
   
   # Recode name of 'not_assessed' for form and reorder to appear first
   options <-
@@ -19,39 +10,17 @@ form_ui <- function(id, criteria_data, levels, options) {
     arrange(value != "not_assessed") |>
     deframe()
   
-  # Function to create form row
-  form_row <- function(id, criteria) {
-    tagList(
-      selectInput(
-        inputId = id,
+  selectInput(
+        inputId = ns("form"),
         label = criteria,
         choices = options,
         selected = "Select progress"
-      )
-    )
-  }
-  
-  tagList(
-    map(
-      unique(criteria_data$level),
-      function(x) {
-        tagList(
-          pmap(criteria_data |> 
-                 filter(level == x) |> 
-                 mutate(id = ns(id)) |>
-                 select(id, criteria), 
-               form_row
-          )
-        )
-      }
-    )      
-  )  
+  )
+
 }
 
-form_server <- function(input, output, session, criteria_id) {
+form_server <- function(input, output, session) {
   
-  return(reactive(
-    map(set_names(criteria_id), ~ input[[.x]])
-  ))
+  return(reactive(input$form))
   
 }
