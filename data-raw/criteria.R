392b35ee7859e414cdb793f1a82d6d3ff6b8c7a9
alicebyers5 <alice.byers@gov.scot>
2024-05-07 16:45:32 +0100

    Add new criteria

--- data-raw/criteria.R
+++ data-raw/criteria.R
@@ -13,13 +13,15 @@ criteria <- tibble::tribble(
   "1_6", "Code published publicly on GitHub", 
   "1_7", "Code reviewed by somebody inside the immediate team", 
   "1_8", "More detailed documentation",
-  "2_1", "Functions used where appropriate", 
-  "2_2", "Dependencies documented",
-  "2_3", "Code reviewed by somebody outside the immediate team",
+  "2_1", "Functions used where appropriate",
+  "2_2", "Data quality assurance built into code",
+  "2_3", "Dependencies documented",
+  "2_4", "Code reviewed by somebody outside the immediate team",
   "3_1", "Re-usable components are packaged", 
   "3_2", "Unit tests for functions", 
   "3_3", "Automated testing; e.g. using GitHub actions", 
-  "3_4", "Computational environment documented"
+  "3_4", "Computational environment documented",
+  "3_5", "Fully automated end-to-end pipeline"
 ) |>
   dplyr::mutate(level = substr(id, 1, 1), .before = criteria) |>
   dplyr::mutate(id = paste0("id_", id))
