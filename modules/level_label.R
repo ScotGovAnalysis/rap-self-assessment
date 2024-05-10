66f89c03c6543cdc4b2758f1a9f238209e10268e
alicebyers5 <alice.byers@gov.scot>
2024-05-10 10:26:25 +0100

    Add margin to download buttons

--- modules/level_label.R
+++ modules/level_label.R
@@ -1,16 +1,14 @@
 # This is a general function rather than a Shiny module
 
-level_label <- function(level, level_options, case = c("upper", "lower")) {
+level_label <- function(level, level_options, rap = TRUE) {
   
-  case <- match.arg(case)
-    
   if(any(!level %in% level_options)) {
     stop("Level provided is not in options")
   }
   
-  paste(
-    ifelse(case == "upper", "Level", "level"), 
-    level, "-", 
-    names(level_options[level_options == level]), "RAP"
+  paste0(
+    names(level_options[level_options == level]), 
+    if(rap) " RAP" else ""
   )
+  
 }
